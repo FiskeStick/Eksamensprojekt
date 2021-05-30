b563356c39510ff62e19f1b90c7662083a16e60c
Stefan <seje26597@edu.ucl.dk>
2021-05-30 16:31:30 +0200

    implemented Jakob's Stuff

--- qa.js
+++ qa.js
@@ -44,3 +44,14 @@ function sec5() {
 
 
 // Stefan E.J
+
+function on() {
+  document.getElementById('overlay').style.display = "block";
+}
+function off() {
+  document.getElementById('overlay').style.display = "none";
+}
+
+
+
+//Jakob
