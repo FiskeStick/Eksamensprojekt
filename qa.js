efe3aa4d183a6bef3ad4ffa752e41dd8b4b2e0a5
ChrisHenriksen <74958290+ChrisHenriksen@users.noreply.github.com>
2021-05-30 13:13:07 +0200

    console.log

--- qa.js
+++ qa.js
@@ -1,18 +1,24 @@
-var coll = document.getElementsByClassName("collapsible"); // variabel for at hente class fra HTML
-var i; // variabel i. Kunne også hedde x eller y
+var coll = document.getElementsByClassName("collapsible");
+console.log(coll);
+var i;
 
-for (i = 0; i < coll.length; i++) { // for loop
-  coll[i].addEventListener("click", function() { // når man kliker så aktiveres funktionen
-    this.classList.toggle("active"); // Hvis collapsible er åbent
-    var content = this.nextElementSibling; // variabel content sørger for at næste collapsible sibling kommer med
-    if (content.style.display === "block") { // hvis content er synligt. Tre '===' betyder Strict equality
-      content.style.display = "none"; // så er content usynligt
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
     } else {
-      content.style.display = "block"; // ellers synligt
+      content.style.display = "block";
+console.log(i);
     }
   });
 }
+//med inspiration fra:
+
 // Christian H
+
+
 
 function sec1() {
   var elmnt1 = document.getElementById("container");
